6dea152616ea341802af96cdacbdfc3c50123e6b
Nicolas Tobar <tobar.n@husky.neu.edu>
2018-12-14 23:17:20 -0500

    Before  LayeredPane impl

--- src/Visual/MainPanels/MyCardsPanel.java
+++ src/Visual/MainPanels/MyCardsPanel.java
@@ -3,22 +3,27 @@ package Visual.MainPanels;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Font;
+import java.util.ArrayList;
 
 import javax.swing.*;
 import javax.swing.border.Border;
+
+import Cards.ICard;
 
 
 public class MyCardsPanel extends JPanel {
   private Color MAIA_BG_COLOR;
   private Color MAIA_LOGO_COLOR;
+  private ArrayList<JPanel> cards;
   private JButton addCard;
   private JPanel panel1;
-  private JLayeredPane myPane;
+  private JLayeredPane cardPane;
 
 
   public MyCardsPanel() {
     super();
 
+    this.cards = new ArrayList<>();
 
     this.setPreferredSize(new Dimension(800, 1000));
 
@@ -34,10 +39,27 @@ public class MyCardsPanel extends JPanel {
             "My Cards", 0, 0, Font.getFont(Font.DIALOG),
             MAIA_LOGO_COLOR));
 
-    
+
+    cardPane = new JLayeredPane();
+    cardPane.setPreferredSize(new Dimension(600, 800));
+    cardPane.setBorder(BorderFactory.createTitledBorder(
+            "My Card Stack---"));
+
 
 
 
 
   }
+
+  public void updateCards() {
+    int size = this.cards.size();
+
+    for(int i = 0; i < size; i++) {
+      cardPane.add(this.cards.get(i), new Integer(i));
+
+    }
+
+
+  }
+
 }
