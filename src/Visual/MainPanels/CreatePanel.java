e4c6cbaeefff1c6795300321fa6d9180d0681d4f
Nicolas Tobar <tobar.n@husky.neu.edu>
2018-12-19 12:21:14 -0500

    JFrame location more precise, but everything closes when JFrame closes

--- src/Visual/MainPanels/CreatePanel.java
+++ src/Visual/MainPanels/CreatePanel.java
@@ -645,6 +645,7 @@ public class CreatePanel extends JPanel implements MouseListener {
     JFrame previewFrame = new JFrame("Preview");
     previewFrame.setSize(new Dimension(previewPanel.getWidth(),previewPanel.getHeight()));
     previewFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    previewFrame.setLocationRelativeTo(null);
     //previewFrame.setPreferredSize(new Dimension(300,500));
     previewFrame.add(previewPanel);
     previewFrame.setVisible(true);
