65b6bda76ffd362e938da7bdfcb48a53dc286286
Nicolas Tobar <tobar.n@husky.neu.edu>
2018-12-20 11:33:16 -0500

    Added specifications for class

--- src/daployer/daployer.java
+++ src/daployer/daployer.java
@@ -1,4 +1,22 @@
 package daployer;
 
-public class daployerg {
+import javax.swing.*;
+
+/**
+ * 1 JPANEL blank for now, linear layout
+ * 1 JPanel with Deploy options with contracts, linear layout
+ * 1 JPanel with Interact options with same contracts, lienar layout
+ * 1 JPanel where contracts are deployed/interacted with, LEFT
+ * 1 JPanel where you get blockchain hashcode
+ */
+
+
+/**
+ * Represents the main Daployer screen to deploy and interact with Smart Contracts
+ */
+public class daployer extends JFrame {
+
+
+
+
 }
